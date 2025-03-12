543b68f18b7f8f932e5f37bbe5018b5ad7c44fab
Alexander Reznichenko <avreznichenko_2@edu.hse.ru>
2025-03-12 15:50:48 +0300

    Fix build
    Signed-off-by: Alexander Reznichenko <avreznichenko_2@edu.hse.ru>

--- app/build.sh
+++ app/build.sh
@@ -1,12 +1,12 @@
 mkdir -p cmake/build
 cd cmake/build
-cmake -DBUILD_SHARED_LIBS=yes \
+cmake -DBUILD_SHARED_LIBS=ON \
       -DgRPC_BUILD_TESTS=OFF \
       -DgRPC_PROTOBUF_PROVIDER=module \
       -DgRPC_ABSL_PROVIDER=module \
       -DgRPC_CARES_PROVIDER=module \
       -DgRPC_RE2_PROVIDER=module \
-      -DgRPC_SSL_PROVIDER=module \
+      -DgRPC_SSL_PROVIDER=package \
       -DgRPC_ZLIB_PROVIDER=module \
       -DCMAKE_INSTALL_PREFIX=/app \
       -DRE2_BUILD_TESTING=OFF \
