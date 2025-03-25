61fb1391e8f288314beaf4caff292f468ee78595
Alexander Reznichenko <avreznichenko_2@edu.hse.ru>
2025-03-26 00:25:14 +0300

    Add Writer and WriterClient
    Signed-off-by: Alexander Reznichenko <avreznichenko_2@edu.hse.ru>

---
+++ app/src/Writer.cpp
@@ -0,0 +1,95 @@
+#include "Writer.h"
+#include "SearcherClient.h"
+
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+
+#include <optional>
+#include <functional>
+
+WriterImpl::WriterImpl(const std::string& host, const std::string& port, const std::string& etcd_addr, const std::string& s3_host, const json& indexes_config)
+    : host(host), port(port), etcd_client(etcd_addr), storage_client(s3_host) {
+    storage_client.logIn("user", "password"); // todo
+
+    for (const auto& item : indexes_config) {
+        indexes[item["id"].get<std::string>()] = std::make_shared<IndexHNSWType>(
+            2, 3,
+            std::bind(&WriterImpl::indexUpdateCallback, this, std::placeholders::_1, std::placeholders::_2),
+            2, 2, faiss::MetricType::METRIC_L2
+        );
+    }
+}
+
+grpc::Status WriterImpl::ProcessWriteRequest(grpc::ServerContext* context, const WriteRequest* request, WriteResponse* response) {
+
+    std::vector<float> vec(request->data().vector_data().begin(), request->data().vector_data().end());
+    std::unordered_map<std::string, std::string> attributes(request->data().attributes().begin(), request->data().attributes().end());
+
+    // ----- Testing -----
+    std::cout << "Document received\nid: " << request->data().id() << std::endl;
+    for (float i : vec) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+    for (auto& pair : attributes) {
+        std::cout << "{" << pair.first << ": " << pair.second << "}" << std::endl;
+    }
+    // -------------------
+
+    if (attributes.find("index-id") == attributes.end()) {
+    	return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, "Missing index id");
+    }
+    std::string index_id = attributes.at("index-id");
+    if (indexes.find(index_id) == indexes.end()) {
+      return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, "Wrong index id");
+    }
+
+    if (attributes.find("delete") == attributes.end()) {
+    	indexes[index_id]->add(1, &index_id, vec.data());
+    }
+    else {
+    	indexes[index_id]->erase(1, &index_id);
+    }
+    return grpc::Status::OK;
+}
+
+WriterImpl::~WriterImpl() {
+}
+
+// todo: slow operation, consider using async
+void WriterImpl::indexUpdateCallback(std::vector<size_t>&& ids, std::vector<std::shared_ptr<const SegmentHNSWType>>&& segs) {
+    std::string index_id = "0"; // todo: callback should know index id
+
+    std::vector<size_t> added;
+    std::vector<size_t> deleted;
+    added.reserve(ids.size());
+    deleted.reserve(segs.size());
+    for (auto id : ids) {
+        added.push_back(id);
+        storage_client.delObject(index_id, std::to_string(id));
+
+    }
+    for (const auto& seg : segs) {
+        deleted.push_back(seg->getID());
+        storage_client.delObject(index_id, std::to_string(seg->getID()));
+    }
+
+    auto hosts = etcd_client.ListSearcherHostsByIndexId(index_id);
+    UpdateRequest request;
+    request.mutable_added()->Assign(added.begin(), added.end());
+    request.mutable_deleted()->Assign(deleted.begin(), deleted.end());
+    for (auto& host : hosts) {
+        std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel(host, grpc::InsecureChannelCredentials());
+        SearcherClient client(channel);
+        UpdateResponse response = client.updateIndex(request);
+    }
+}
+
+Writer::Writer(const json& config) :
+    BaseServer(config), service(WriterImpl(host, port, config["etcd_address"].template get_ref<const std::string&>(),
+                                                       config["s3-host"].template get_ref<const std::string&>(),
+                                                       config["indexes"])) {}
+
+void Writer::Run() {
+    InternalRun(service);
+}
